89f2d97f40e76c30963fa8299cec92abed30af01
Yusuf Eren Aydın <70768298+LikeSupreme@users.noreply.github.com>
2022-07-27 14:27:35 +0300

    painful update

--- Business/Abstract/IUserService.cs
+++ Business/Abstract/IUserService.cs
@@ -1,5 +1,5 @@
-﻿using Core.Utilities.Results;
-using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Core.Utilities.Results;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,7 +8,9 @@ namespace Business.Abstract
 {
     public interface IUserService
     {
+        IDataResult<List<OperationClaim>> GetClaims(User user);
         IDataResult<List<User>> GetAll();
+        IDataResult<User> GetByMail(string email);
         IDataResult<User> GetById(int id);
         IResult Add(User user);
         IResult Delete(User user);
